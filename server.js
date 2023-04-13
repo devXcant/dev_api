32614da3252b3f144549629c408d47c1fba9d80a
CantPlayXDev <105569592+CantPlayXDev@users.noreply.github.com>
2023-04-13 15:35:57 +0100

    commit

--- server.js
+++ server.js
@@ -1,10 +1,17 @@
 const express = require('express');
 const dotenv = require('dotenv');
 
+//route files
+const bootcamps = require('./routes/bootcamps')
+
 //load env file
 dotenv.config({path: '../config/config.env'});
 
 const app = express();
+
+//mount routes
+app.use('/api/v1/bootcamps', bootcamps)
+
 
 const PORT = process.env.PORT  || 5000;
 
