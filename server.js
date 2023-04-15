0ed5d5d5958c8e0443a8d0df5b6a5b7901e90704
CantPlayXDev <105569592+CantPlayXDev@users.noreply.github.com>
2023-04-15 11:42:01 +0100

    .

--- server.js
+++ server.js
@@ -1,5 +1,8 @@
 const express = require('express');
 const dotenv = require('dotenv');
+const logger = require('./middleware/logger')
+
+// const morgan = require('morgan')
 
 //route files
 const bootcamps = require('./routes/bootcamps')
@@ -9,6 +12,15 @@ dotenv.config({path: '../config/config.env'});
 
 const app = express();
 
+
+// if(process.env.NODE_ENV === 'development'{
+//   app.use(morgab(''))
+// })
+
+
+
+
+app.use(logger)
 //mount routes
 app.use('/api/v1/bootcamps', bootcamps)
 
